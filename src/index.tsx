ffdecaccde78246cc9f068257a8385d149ca9dc9
kyorohiro <kyorohiro@gmail.com>
2021-09-26 00:19:02 +0900

    add tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css';
 
 const Hello = (props:any)=> {
   return (
     <div>Hello</div>
   )
 }
+
 ReactDOM.render(
   <>
     <Hello/>
